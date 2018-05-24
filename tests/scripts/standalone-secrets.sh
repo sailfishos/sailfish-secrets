48403ea601d4f2b8b476b0af398a9cc085aa336b
Chris Adams <chris.adams@jollamobile.com>
2018-05-24 17:21:15 +1000

    Add more manual test scripts

---
+++ tests/scripts/standalone-secrets.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This script must be run within a devel-su -p shell
+# The daemon must be running in autotest mode (with --test option)
+
+# TODO: this script should test --store-standalone-secret etc.
